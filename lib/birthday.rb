37314a06477b48abeab7e528e23025ffe2f22447
Kelliann Lafferty <kell10192@gmail.com>
2019-06-17 05:07:55 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -11,4 +11,22 @@ def happy_birthday(birthday_kids)
 end
 
 def age_appropriate_birthday(birthday_kids)
+  birthday_kids each do |kids_name, age|
+    if age <= 12 
+      puts "You are too old for this."
+    else 
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    end
+  end
   
+  
+
+ winner = ""
+  passengers.each do |suite, name|
+    if suite == :suite_a &&
+      name.start_with?("A")
+      winner = name
+    end
+end
+winner
+end
